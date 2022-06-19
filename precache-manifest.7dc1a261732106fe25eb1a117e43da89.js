15bded9a502df925261a19d93cd0271431be457c
abdul <abdulramiz007@gmail.com>
2022-06-19 12:55:36 +0530

    Updates

---
+++ precache-manifest.7dc1a261732106fe25eb1a117e43da89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5c6d12decb279897f309",
+    "url": "/static/css/main.fa8a95db.chunk.css"
+  },
+  {
+    "revision": "5c6d12decb279897f309",
+    "url": "/static/js/main.5c6d12de.chunk.js"
+  },
+  {
+    "revision": "0b5adef5a8e99730695f",
+    "url": "/static/js/1.0b5adef5.chunk.js"
+  },
+  {
+    "revision": "1bc81d8581df27668daf",
+    "url": "/static/js/2.1bc81d85.chunk.js"
+  },
+  {
+    "revision": "a95ebad08de026ef45af",
+    "url": "/static/js/runtime~main.a95ebad0.js"
+  },
+  {
+    "revision": "19a0fb03e2733a43174b2e9a0ef7d474",
+    "url": "/index.html"
+  }
+];
